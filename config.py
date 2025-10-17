79d53f0d13142b21a001ccdd3c2024b233bdfd1f
geetasaini2042 <Geetasaini2042@gmail.com>
2025-10-17 12:03:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
+api_id = 24673538
+api_hash = "555639745e6ceee1ae3797866136998f"
+bot_token = "8498552239:AAFQjLU6DePHb5sPvAFD7DR5sUCwsXdIFIc"
 auth_users = [6150091802]
